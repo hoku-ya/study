976eefcfbef0475c938c7c135d913437724e008b
yamaguchi <ts8807701@gmail.com>
2023-10-07 20:29:39 +0900

    Added description about stdin

--- shellscript/foundation/stdin.sh
+++ shellscript/foundation/stdin.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+read var
+echo var = $var
+
+read var1 var2 var3
+echo var1 = $var1, var2 = $var2, var3 = $var3
+
+read -p 'Input var: ' var
+echo var = $var
+
+read -sp 'password: ' password
+echo password = $password
+
+read -a names
+echo names = ${names[@]}
